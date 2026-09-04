2703ac702b5db1980f9749dc3b53240111ce9c4a
Niteshwar Kumar <162494389+Niteshwarnikku@users.noreply.github.com>
2026-09-04 23:45:21 +0530

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int, int>> maxHeap;
+
+        for(int i = 0; i < arr.size(); i++) {
+
+            int diff = abs(x - arr[i]);
+
+            maxHeap.push({diff, arr[i]});
+
+            if(maxHeap.size() > k) {
+                maxHeap.pop();
+            }
+        }
+
+        vector<int>res;
+        while(!maxHeap.empty()){
+            res.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
